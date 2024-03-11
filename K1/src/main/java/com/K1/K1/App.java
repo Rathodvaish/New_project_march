a7a9f279ccfa73f2a3e6b74a82cf78425fd97c10
IND <IND@DESKTOP-7CPHJ32>
2024-03-11 19:03:03 +0530

    [K1 commit]

---
+++ K1/src/main/java/com/K1/K1/App.java
@@ -0,0 +1,13 @@
+package com.K1.K1;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ K1/src/test/java/AllFeature/First.feature
@@ -0,0 +1,8 @@
+Feature: Sample feature
+
+Scenario: print hi
+ * print 'vaishnavi'
+ * print 'i am vaish'
+ * def value= 10
+ * print value  
+
